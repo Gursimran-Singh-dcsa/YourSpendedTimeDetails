e57c4c9b4ae667dad7f0d1c7549064bca0248b27
gursimran singh <gursimransingh8075@gmail.com>
2019-07-30 12:21:30 +0530

    add basic func

--- timeCounter.js
+++ timeCounter.js
@@ -1,13 +1,65 @@
 var timeCounter = /** @class */ (function () {
     function timeCounter() {
+        var _this = this;
+        this.startTime = [];
+        this.pauseTime = [];
+        this.restartTime = [];
+        this.stopTime = [];
+        this.previousTotal = [];
+        this.ticking = true;
+        this.init = function () {
+            _this.hours = 0;
+            _this.minutes = 0;
+            _this.seconds = 0;
+            _this.startTime = [];
+            _this.setInput("id", "counter", "00:00:00");
+        };
         this.startHandler = function () {
-            console.log('start handler invoked');
+            _this.ticking = true;
+            clearInterval(_this.timeInterval);
+            _this.init();
+            _this.startTime.push(_this.timeGetter());
+            console.log(_this.startTime);
+            _this.countTime();
         };
         this.resumeHandler = function () {
-            console.log('resume handler invoked');
+            _this.ticking = !_this.ticking;
+            if (_this.ticking) {
+                document.getElementById("resume").innerHTML = "Pause";
+                _this.restartTime.push(_this.timeGetter());
+                _this.countTime();
+            }
+            else {
+                clearInterval(_this.timeInterval);
+                document.getElementById("resume").innerHTML = "Resume";
+                _this.pauseTime.push(_this.timeGetter());
+            }
         };
         this.stopHandler = function () {
-            console.log('resume handler invoked');
+            clearInterval(_this.timeInterval);
+            _this.init();
+        };
+        this.getTimeString = function () {
+            return ((_this.hours < 10 ? '0' : '') + _this.hours + (_this.minutes < 10 ? ':0' : ':') + _this.minutes + (_this.seconds < 10 ? ':0' : ':') + _this.seconds);
+        };
+        this.setInput = function (Property, PropertyValue, value) {
+            switch (Property) {
+                case 'id':
+                    document.getElementById(PropertyValue).value = value;
+                    break;
+            }
+        };
+        this.timeGetter = function () {
+            var date = new Date();
+            _this.hours = date.getHours();
+            _this.minutes = date.getMinutes();
+            _this.seconds = date.getSeconds();
+            return _this.getTimeString();
+        };
+        this.countTime = function () {
+            _this.timeInterval = setInterval(function () {
+                console.log('startTime ', tc.startTime, ' pauseTime ', tc.pauseTime, ' restartTime ', tc.restartTime, ' stopTime ', tc.stopTime);
+            }, 1000);
         };
     }
     return timeCounter;
--- timeCounter.ts
+++ timeCounter.ts
@@ -1,19 +1,75 @@
 class timeCounter {
 
-  startTime = [];
-  pauseTime = [];
-  restartTime = [];
-  stopTime = [];
+  hours:number;
+  minutes:number;
+  seconds:number;
+  startTime:any = [];
+  pauseTime:any = [];
+  restartTime:any = [];
+  stopTime:any = [];
+  previousTotal:any = [];
+  timeInterval:any;
+  ticking:boolean = true;
+
+  init = () => {
+    this.hours = 0;
+    this.minutes = 0;
+    this.seconds = 0;
+    this.startTime = [];
+    this.setInput("id", "counter", "00:00:00");
+  }
+
   startHandler = () => {
-    console.log('start handler invoked');
+    this.ticking = true;
+    clearInterval(this.timeInterval);
+    this.init();
+    this.startTime.push(this.timeGetter());
+    console.log(this.startTime);
+    this.countTime();
   }
 
   resumeHandler = () => {
-    console.log('resume handler invoked');
+    this.ticking = !this.ticking;
+    if(this.ticking) {
+      document.getElementById("resume").innerHTML = "Pause";
+      this.restartTime.push(this.timeGetter());
+      this.countTime();
+    } else {
+      clearInterval(this.timeInterval);
+      document.getElementById("resume").innerHTML = "Resume";
+      this.pauseTime.push(this.timeGetter());
+    }
   }
 
   stopHandler = () => {
-    console.log('resume handler invoked');
+    clearInterval(this.timeInterval);
+    this.init();
+  }
+
+  getTimeString = () => {
+    return ((this.hours < 10 ? '0' : '') + this.hours + (this.minutes < 10 ? ':0' : ':') + this.minutes + (this.seconds < 10 ? ':0' : ':') + this.seconds);
+  }
+
+  setInput = (Property:string, PropertyValue:string, value:string) => {
+    switch(Property) {
+      case 'id': 
+        (<HTMLInputElement>document.getElementById(PropertyValue)).value = value;
+        break;
+    }
+  }
+  timeGetter = () => {
+    var date = new Date();
+    this.hours = date.getHours();
+    this.minutes = date.getMinutes();
+    this.seconds = date.getSeconds();
+    return this.getTimeString();
+  }
+
+  countTime = () => {
+    this.timeInterval = setInterval(
+      function() {
+        console.log('startTime ', tc.startTime, ' pauseTime ', tc.pauseTime, ' restartTime ', tc.restartTime, ' stopTime ', tc.stopTime);
+      },1000);
   }
 
 }
