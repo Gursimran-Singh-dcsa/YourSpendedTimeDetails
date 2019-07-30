69ac7f9341cbe8d57326cd6d87a1a92348e3db0e
gursimran singh <gursimransingh8075@gmail.com>
2019-07-30 16:28:04 +0530

    fully-functional

--- timeCounter.ts
+++ timeCounter.ts
@@ -7,7 +7,6 @@ class timeCounter {
   pauseTime:any = [];
   restartTime:any = [];
   stopTime:any = [];
-  previousTotal:any = [];
   timeInterval:any;
   ticking:boolean = true;
 
@@ -15,7 +14,9 @@ class timeCounter {
     this.hours = 0;
     this.minutes = 0;
     this.seconds = 0;
-    this.startTime = [];
+    this.startTime.push(this.timeGetter());
+    this.restartTime = this.startTime;
+    this.pauseTime.push(this.timeGetter());
     this.setInput("id", "counter", "00:00:00");
   }
 
@@ -23,8 +24,6 @@ class timeCounter {
     this.ticking = true;
     clearInterval(this.timeInterval);
     this.init();
-    this.startTime.push(this.timeGetter());
-    console.log(this.startTime);
     this.countTime();
   }
 
@@ -68,13 +67,79 @@ class timeCounter {
   countTime = () => {
     this.timeInterval = setInterval(
       function() {
-        console.log('startTime ', tc.startTime, ' pauseTime ', tc.pauseTime, ' restartTime ', tc.restartTime, ' stopTime ', tc.stopTime);
+         tc.getOutput()
       },1000);
+  }
+
+  getOutput = () => {
+    var currentTime = this.timeGetter();
+    var totalDiff = this.getDiff( this.startTime[0], currentTime);
+    var totalTime = '00:00:00';
+    for(var i=0;i<this.restartTime.length;i++) {
+      totalTime = this.add( totalTime, this.getDiff(this.pauseTime[i], this.restartTime[i]));
+    }
+    if(this.pauseTime.length > this.restartTime.length) {
+      totalTime = this.add(totalTime, this.getDiff(this.pauseTime[this.pauseTime.length], currentTime));
+    }
+    totalDiff = this.getDiff(totalTime, totalDiff);
+    this.setInput("id", "counter", totalDiff);
+  }
+
+  getDiff = (timeString1, timeString2) => {
+    const [h1, m1, s1] = this.timeBreaker(timeString1);
+    const [h2, m2, s2] = this.timeBreaker(timeString2);
+    if(this.validateTime(h1, m1, s1, h2, m2, s2)) {
+      var totalStartSec:number = (h1 * 3600) + (m1 * 60) + s1;
+      var totalEndSec:number = (h2 * 3600) + (m2 * 60) + s2;
+      var timeDiffInSec:number = totalEndSec - totalStartSec;
+      var hourDiff:number = Math.floor(timeDiffInSec / 3600);
+      var minDiff:number = timeDiffInSec % 3600;
+      minDiff = Math.floor(minDiff / 60);
+      var secDiff:number = (timeDiffInSec % 60);
+      this.hours = hourDiff;
+      this.minutes = minDiff;
+      this.seconds =  secDiff;
+      return this.getTimeString();
+    } else {
+      clearInterval(this.timeInterval);
+      return '00:00:00';
+    }
+  }
+
+  add = (timeString1, timeString2) => {
+    const [h1, m1, s1] = this.timeBreaker(timeString1);
+    const [h2, m2, s2] = this.timeBreaker(timeString2);
+    var totalStartSec:number = (h1 * 3600) + (m1 * 60) + s1;
+    var totalEndSec:number = (h2 * 3600) + (m2 * 60) + s2;
+    var totalTimeInSec:number = totalEndSec + totalStartSec;
+    var totalHour:number = Math.floor(totalTimeInSec / 3600);
+    var totalMin:number = totalTimeInSec % 3600;
+    totalMin = Math.floor(totalMin / 60);
+    var totalSec:number = (totalTimeInSec % 60);
+    this.hours = totalHour;
+    this.minutes = totalMin;
+    this.seconds =  totalSec;
+    return this.getTimeString();
+  }
+
+  timeBreaker(time:string)
+  {
+    return ([parseInt(time.slice(0,2)), parseInt(time.slice(3,5)), parseInt((time.slice(6,8) == "") ? "0" : time.slice(6,8))])
+  }
+
+  validateTime(startHour:number, startMin:number, startSec:number, endHour:number, endMin:number, endSec:number): boolean {
+    var timeIsValid = true;
+    if ((startHour > endHour) || (startHour == endHour && startMin > endMin) || (startHour == endHour && startMin == endMin && startSec > endSec)) {
+      timeIsValid = false;
+      return timeIsValid;
+    }
+    return timeIsValid;
   }
 
 }
 
 var tc = new timeCounter();
+tc.init();
 const startHandler = () => {
   tc.startHandler();
 }
